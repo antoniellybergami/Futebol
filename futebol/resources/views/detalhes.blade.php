b30b14906c41b7083ecd0474d599d9c6a83f27aa
Antonielly <antoniellybergami@gmail.com>
2024-10-24 15:03:03 -0300

    front basico

--- futebol/resources/views/detalhes.blade.php
+++ futebol/resources/views/detalhes.blade.php
@@ -9,135 +9,163 @@
         $cartoesVermelhosVisitante = json_decode($detalhes->cartao_vermelho_visitante);
     @endphp
 
-    <h1>Detalhes da Partida</h1>
+    <h1 class="titulo">Detalhes da Partida</h1>
 
-    <h3>Rodada: {{ $detalhes->campeonato ?? 'Informações não disponíveis' }}</h3>
-    <h2>Placar: {{ $detalhes->placar ?? 'Informações não disponíveis' }}</h2>
-    <h3>Estádio: {{ $detalhes->estadio ?? 'Informações não disponíveis' }}</h3>
-    <h3>Status: {{ $detalhes->status ?? 'Informações não disponíveis' }}</h3>
-    <h3>Rodada: {{ $detalhes->rodada ?? 'Informações não disponíveis' }}</h3>
+    <h2 class="placar">{{ $detalhes->placar ?? 'Informações não disponíveis' }}</h2>
+    <h3 class="subtitulo">Campeonato: {{ $detalhes->campeonato ?? 'Informações não disponíveis' }}</h3>
+    <h3 class="subtitulo">Estádio: {{ $detalhes->estadio ?? 'Informações não disponíveis' }}</h3>
+    <h3 class="subtitulo">Status: {{ $detalhes->status ?? 'Informações não disponíveis' }}</h3>
+    <h3 class="subtitulo">Rodada: {{ $detalhes->rodada ?? 'Informações não disponíveis' }}</h3>
 
-    <h3>Informações do Time Mandante</h3>
+    <div class="info-time">
+        <h3>Informações do Time Mandante</h3>
 
-    <h4>Gols do Mandante:</h4>
-    <ul>
-        @if (!empty($gols->mandante))
-            @foreach ($gols->mandante as $gol)
-                <li>{{ $gol->atleta->nome_popular ?? 'Informações não disponíveis' }} minuto {{ $gol->minuto ?? 'Informações não disponíveis' }}</li>
-            @endforeach
-        @else
-            <li>Sem gols</li>
-        @endif
-    </ul>
-    
-    <h4>Escalação Tática: {{ $escalacaoMandante->esquema_tatico ?? 'Informações não disponíveis' }}</h4>
-    <h4>Técnico: {{ $escalacaoMandante->tecnico->nome_popular ?? 'Informações não disponíveis' }}</h4>
+        <h4>Gols do Mandante:</h4>
+        <ul>
+            @if (!empty($gols->mandante))
+                @foreach ($gols->mandante as $gol)
+                    <li>{{ $gol->atleta->nome_popular ?? 'Informações não disponíveis' }} - minuto {{ $gol->minuto ?? 'Informações não disponíveis' }}</li>
+                @endforeach
+            @else
+                <li>Sem gols</li>
+            @endif
+        </ul>
+        
+        <h4>Escalação Tática: {{ $escalacaoMandante->esquema_tatico ?? 'Informações não disponíveis' }}</h4>
+        <h4>Técnico: {{ $escalacaoMandante->tecnico->nome_popular ?? 'Informações não disponíveis' }}</h4>
 
-    <h3>Escalação Mandante:</h3>
-    <h5>Jogadores Escalados:</h5>
-    <ul>
-        @if (!empty($escalacaoMandante->titulares))
-            @foreach ($escalacaoMandante->titulares as $jogador)
-                <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
-            @endforeach
-        @else
-            <li>Informações não disponíveis</li>
-        @endif
-    </ul>
+        <h3>Escalação Mandante:</h3>
+        <h5>Jogadores Escalados:</h5>
+        <ul>
+            @if (!empty($escalacaoMandante->titulares))
+                @foreach ($escalacaoMandante->titulares as $jogador)
+                    <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
+                @endforeach
+            @else
+                <li>Informações não disponíveis</li>
+            @endif
+        </ul>
 
-    <h5>Reservas:</h5>
-    <ul>
-        @if (!empty($escalacaoMandante->reservas))
-            @foreach ($escalacaoMandante->reservas as $jogador)
-                <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
-            @endforeach
-        @else
-            <li>Informações não disponíveis</li>
-        @endif
-    </ul>
+        <h5>Reservas:</h5>
+        <ul>
+            @if (!empty($escalacaoMandante->reservas))
+                @foreach ($escalacaoMandante->reservas as $jogador)
+                    <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
+                @endforeach
+            @else
+                <li>Informações não disponíveis</li>
+            @endif
+        </ul>
 
-    <h3>Cartões Amarelos (Mandante):</h3>
-    <ul>
-        @if (!empty($cartoesAmarelosMandante))
-            @foreach ($cartoesAmarelosMandante as $cartao)
-                <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
-            @endforeach
-        @else
-            <li>Não há cartões amarelos registrados</li>
-        @endif
-    </ul>
+        <h3>Cartões Amarelos (Mandante):</h3>
+        <ul>
+            @if (!empty($cartoesAmarelosMandante))
+                @foreach ($cartoesAmarelosMandante as $cartao)
+                    <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
+                @endforeach
+            @else
+                <li>Não há cartões amarelos registrados</li>
+            @endif
+        </ul>
 
-  
+        <h3>Cartões Vermelhos (Mandante):</h3>
+        <ul>
+            @if (!empty($cartoesVermelhosMandante))
+                @foreach ($cartoesVermelhosMandante as $cartao)
+                    <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
+                @endforeach
+            @else
+                <li>Não há cartões vermelhos registrados</li>
+            @endif
+        </ul>
+    </div>
 
-    <h3>Cartões Vermelhos (Mandante):</h3>
-    <ul>
-        @if (!empty($cartoesVermelhosMandante))
-            @foreach ($cartoesVermelhosMandante as $cartao)
-                <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
-            @endforeach
-        @else
-            <li>Não há cartões vermelhos registrados</li>
-        @endif
-    </ul>
+    <div class="info-time">
+        <h3>Informações Visitantes</h3>
 
-    <h3>Informações Visitantes</h3>
+        <h4>Gols do Visitante:</h4>
+        <ul>
+            @if (!empty($gols->visitante))
+                @foreach ($gols->visitante as $gol)
+                    <li>{{ $gol->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $gol->minuto ?? 'Minuto não disponível' }}</li>
+                @endforeach
+            @else
+                <li>Não há gols registrados para o visitante.</li>
+            @endif
+        </ul>
 
-    <h4>Gols do Visitante:</h4>
-    <ul>
-        @if (!empty($gols->visitante))
-            @foreach ($gols->visitante as $gol)
-                <li>{{ $gol->atleta->nome_popular ?? 'Nome não disponível' }} minuto {{ $gol->minuto ?? 'Minuto não disponível' }}</li>
-            @endforeach
-        @else
-            <li>Não há gols registrados para o visitante.</li>
-        @endif
-    </ul>
+        <h3>Escalação Visitante:</h3>
+        <h4>Escalação Tática: {{ $escalacaoVisitante->esquema_tatico ?? 'Informações não disponíveis' }}</h4>
+        <h4>Técnico: {{ $escalacaoVisitante->tecnico->nome_popular ?? 'Informações não disponíveis' }}</h4>
 
-    <h3>Escalação Visitante:</h3>
-    <h4>Escalação Tática: {{ $escalacaoVisitante->esquema_tatico ?? 'Informações não disponíveis' }}</h4>
-    <h4>Técnico: {{ $escalacaoVisitante->tecnico->nome_popular ?? 'Informações não disponíveis' }}</h4>
+        <h5>Jogadores Escalados:</h5>
+        <ul>
+            @if (!empty($escalacaoVisitante->titulares))
+                @foreach ($escalacaoVisitante->titulares as $jogador)
+                    <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
+                @endforeach
+            @else
+                <li>Informações não disponíveis</li>
+            @endif
+        </ul>
 
-    <h5>Jogadores Escalados:</h5>
-    <ul>
-        @if (!empty($escalacaoVisitante->titulares))
-            @foreach ($escalacaoVisitante->titulares as $jogador)
-                <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
-            @endforeach
-        @else
-            <li>Informações não disponíveis</li>
-        @endif
-    </ul>
+        <h5>Reservas:</h5>
+        <ul>
+            @if (!empty($escalacaoVisitante->reservas))
+                @foreach ($escalacaoVisitante->reservas as $jogador)
+                    <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
+                @endforeach
+            @else
+                <li>Informações não disponíveis</li>
+            @endif
+        </ul>
 
-    <h5>Reservas:</h5>
-    <ul>
-        @if (!empty($escalacaoVisitante->reservas))
-            @foreach ($escalacaoVisitante->reservas as $jogador)
-                <li>{{ $jogador->camisa ?? 'Sem Camisa' }} - {{ $jogador->atleta->nome_popular ?? 'Sem Nome' }}: {{ $jogador->posicao->nome ?? 'Sem Posição' }}</li>
-            @endforeach
-        @else
-            <li>Informações não disponíveis</li>
-        @endif
-    </ul>
+        <h3>Cartões Amarelos (Visitante):</h3>
+        <ul>
+            @if (!empty($cartoesAmarelosVisitante))
+                @foreach ($cartoesAmarelosVisitante as $cartao)
+                    <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
+                @endforeach
+            @else
+                <li>Não há cartões amarelos registrados</li>
+            @endif
+        </ul>
 
-    <h3>Cartões Amarelos (Visitante):</h3>
-    <ul>
-        @if (!empty($cartoesAmarelosVisitante))
-            @foreach ($cartoesAmarelosVisitante as $cartao)
-                <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
-            @endforeach
-        @else
-            <li>Não há cartões amarelos registrados</li>
-        @endif
-    </ul>
-
-    <h3>Cartões Vermelhos (Visitante):</h3>
-    <ul>
-        @if (!empty($cartoesVermelhosVisitante))
-            @foreach ($cartoesVermelhosVisitante as $cartao)
-                <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
-            @endforeach
-        @else
-            <li>Não há cartões vermelhos registrados</li>
-        @endif
-    </ul>
+        <h3>Cartões Vermelhos (Visitante):</h3>
+        <ul>
+            @if (!empty($cartoesVermelhosVisitante))
+                @foreach ($cartoesVermelhosVisitante as $cartao)
+                    <li>{{ $cartao->atleta->nome_popular ?? 'Nome não disponível' }} - minuto {{ $cartao->minuto ?? 'Minuto não disponível' }}</li>
+                @endforeach
+            @else
+                <li>Não há cartões vermelhos registrados</li>
+            @endif
+        </ul>
+    </div>
 </div>
+
+<style>
+    .container {
+        margin: 0 auto;
+        padding: 20px;
+    }
+
+    .titulo {
+        text-align: center;
+    }
+
+    .subtitulo {
+        color: #555;
+    }
+
+    .placar {
+        font-size: 1.5em;
+        color: red; 
+        text-align: center;
+    }
+
+    .info-time {
+        margin-bottom: 20px; 
+        padding: 15px;
+        border: 2px solid #ddd;
+    }
--- futebol/resources/views/todasAsPartidas.blade.php
+++ futebol/resources/views/todasAsPartidas.blade.php
@@ -5,16 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todas as Partidas</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}"> <!-- Adicione o CSS se necessário -->
+    <style>
+        /* Estilos para a tabela */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 10px; /* Espaçamento interno */
+            text-align: left;
+            border-bottom: 1px solid #ddd; /* Linha de separação entre as linhas */
+        }
+
+        /* Estilo para espaçamento entre escudos e nomes */
+        .partida {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .escudo {
+            margin: 0 10px; /* Margem horizontal para espaçamento */
+        }
+    </style>
 </head>
 <body>
     <h1>Todas as Partidas</h1>
     <table>
         <thead>
             <tr>
-                <th>Time Casa</th>
-                <th>Escudo Casa</th>
-                <th>Time Fora</th>
-                <th>Escudo Fora</th>
+                <th>Partida</th>
                 <th>Status</th>
                 <th>Data</th>
                 <th>Hora</th>
@@ -24,16 +46,15 @@
         <tbody>
             @foreach($partidas as $partida)
                 <tr>
-                    <td>{{ $partida->time_casa }}</td>
-                    <td><img src="{{ $partida->escudo_casa }}" alt="Escudo {{ $partida->time_casa }}" width="50"></td>
-                    <td>{{ $partida->time_fora }}</td>
-                    <td><img src="{{ $partida->escudo_fora }}" alt="Escudo {{ $partida->time_fora }}" width="50"></td>
+                    <td class="partida">
+                        <img class="escudo" src="{{ $partida->escudo_casa }}" alt="Escudo {{ $partida->time_casa }}" width="30"> 
+                        {{ $partida->time_casa }} X {{ $partida->time_fora }} 
+                        <img class="escudo" src="{{ $partida->escudo_fora }}" alt="Escudo {{ $partida->time_fora }}" width="30">
+                    </td>
                     <td>{{ $partida->status }}</td>
                     <td>{{ $partida->data ?? 'N/A' }}</td> 
                     <td>{{ $partida->hora ?? 'N/A' }}</td> 
                     <td><a href="{{ route('partidas.saveDetalhes', $partida->partida_id) }}">Ver Detalhes do Jogo</a></td>
-
-                    </td>
                 </tr>
             @endforeach
         </tbody>
